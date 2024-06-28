97cfe7fe0d3597e5ef453e5de5c47e6a279c5602
cami0906 <gjohan465@gmail.com>
2024-06-27 21:39:02 -0500

    componenetes dinamicos y estaticos actualizados creacion de la carpeta service

--- frontend_facturacion/proyecto_jas/src/app/app.routes.ts
+++ frontend_facturacion/proyecto_jas/src/app/app.routes.ts
@@ -1,18 +1,17 @@
 import { Routes } from '@angular/router';
-import { InicioComponent } from './components/admin/inicio/inicio.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { AgregarProductoComponent } from './components/admin/agregar-producto/agregar-producto.component';
 import { EstadisticasComponent } from './components/admin/estadisticas/estadisticas.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
+import { PaginaPrincipalComponent } from './components/pagina-principal/pagina-principal.component';
+import { InicioComponent } from './components/admin/inicio/inicio.component';
 
 
 
 export const routes: Routes = [
-{path:"", component:InicioComponent },
-{path: "", component:NavBarComponent},
-{path: "", component:AgregarProductoComponent},
-{path:  "", component:EstadisticasComponent},
-{ path:  "", component: ContactoComponent }
+    { path: "inicio", component: InicioComponent },
+    { path: "crear-producto", component: AgregarProductoComponent },
+    { path: "estadisticas", component: EstadisticasComponent },
+    { path: "contacto", component: ContactoComponent }
 
 
 ];
---
+++ frontend_facturacion/proyecto_jas/src/app/services/api-general.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ApiGeneralService {
+    seLogueo: any = false
+
+    constructor() { }
+
+    obtenerStatusLogin(){
+        return this.seLogueo
+    }
+}
