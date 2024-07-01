2bfa9d8cc906da3edbcbbde7ac4d3bfa128e0f3f
felipe currea <afelipe096@hotmail.com>
2024-07-01 14:19:49 -0500

    actualizacion de pagina de inicio

--- frontend_facturacion/proyecto_jas/src/app/components/pagina-principal/pagina-principal.component.spec.ts
+++ frontend_facturacion/proyecto_jas/src/app/components/pagina-principal/pagina-principal.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PaginaPrincipalComponent } from './pagina-principal.component';
 
 describe('PaginaPrincipalComponent', () => {
-  let component: PaginaPrincipalComponent;
-  let fixture: ComponentFixture<PaginaPrincipalComponent>;
+    let component: PaginaPrincipalComponent;
+    let fixture: ComponentFixture<PaginaPrincipalComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [PaginaPrincipalComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [PaginaPrincipalComponent]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(PaginaPrincipalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(PaginaPrincipalComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
