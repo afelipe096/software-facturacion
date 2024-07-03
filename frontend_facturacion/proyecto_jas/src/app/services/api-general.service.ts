4e026b4a79dd21f93230a0cd40923ca3bbea1295
cami0906 <gjohan465@gmail.com>
2024-07-02 19:07:41 -0500

    actual

--- frontend_facturacion/proyecto_jas/src/app/services/api-general.service.ts
+++ frontend_facturacion/proyecto_jas/src/app/services/api-general.service.ts
@@ -4,7 +4,9 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 export class ApiGeneralService {
-    seLogueo: any = true
+    seLogueo: any = false
+
+    
 
     constructor() { }
 
