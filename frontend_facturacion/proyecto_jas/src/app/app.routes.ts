afee415391ef1ca0bf45855a961e0d314d909f02
cami0906 <gjohan465@gmail.com>
2024-07-03 19:38:14 -0500

    upadte finish

--- frontend_facturacion/proyecto_jas/src/app/app.routes.ts
+++ frontend_facturacion/proyecto_jas/src/app/app.routes.ts
@@ -2,14 +2,13 @@ import { Routes } from '@angular/router';
 import { AgregarProductoComponent } from './components/admin/agregar-producto/agregar-producto.component';
 import { EstadisticasComponent } from './components/admin/estadisticas/estadisticas.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
-import { InicioComponent } from './components/admin/inicio/inicio.component';
 import { InventarioComponent } from './components/admin/inventario/inventario.component';
 
 
 
 
 export const routes: Routes = [
-    { path: "", component: InicioComponent },
+
     { path: "crear-producto", component: AgregarProductoComponent },
     { path: "estadisticas", component: EstadisticasComponent },
     { path: "contacto", component: ContactoComponent },
