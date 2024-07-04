52b918ea4aa36caf9a20908db4397da3f3d9c6d5
felipe currea <afelipe096@hotmail.com>
2024-07-04 18:45:32 -0500

    actualizacio

--- frontend_facturacion/proyecto_jas/src/app/components/admin/estadisticas/estadisticas.component.ts
+++ frontend_facturacion/proyecto_jas/src/app/components/admin/estadisticas/estadisticas.component.ts
@@ -31,7 +31,45 @@ export class EstadisticasComponent implements AfterViewInit {
             data: {
                 labels: ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado','Domingo'],
                 datasets: [{
-                    label: '# of Votes',
+                    label: 'VENTAS',
+                    data: [12, 19, 3, 5, 2, 3,8],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)',
+                        'rgba(355, 59, 4, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(355, 59, 4, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+
+                    }
+                }
+            }
+        });
+
+        const ctx6 = document.getElementById('myChart6') as HTMLCanvasElement;
+        new Chart(ctx6, {
+            type: 'line', // Cambia esto al tipo de gráfico que desees
+            data: {
+                labels: ['10 AM', '11 AM', '12 AM', '1 PM', '2 PM', '3 PM','4 PM'],
+                datasets: [{
+                    label: 'VENTAS X HORA',
                     data: [12, 19, 3, 5, 2, 3,8],
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
@@ -159,6 +197,8 @@ export class EstadisticasComponent implements AfterViewInit {
             },
 
         });
+
+
     }
     }
 
