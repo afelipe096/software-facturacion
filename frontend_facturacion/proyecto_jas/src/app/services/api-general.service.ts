8e452a1d3f6461d72d7b64aad7f327523f5682c4
KiryNatsu27 <sebas4907@gmail.com>
2024-07-03 22:07:32 -0500

    cambios

--- frontend_facturacion/proyecto_jas/src/app/services/api-general.service.ts
+++ frontend_facturacion/proyecto_jas/src/app/services/api-general.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 export class ApiGeneralService {
-    seLogueo: any = false
+    seLogueo: any = true
 
     constructor() { }
 
