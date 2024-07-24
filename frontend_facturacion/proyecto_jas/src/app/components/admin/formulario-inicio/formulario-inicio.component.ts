03460c796ee8a9a60a4aa61937eac8ba4a0f41fe
felipe currea <afelipe096@hotmail.com>
2024-07-23 19:03:55 -0500

    actualizacion puerto

---
+++ frontend_facturacion/proyecto_jas/src/app/components/admin/formulario-inicio/formulario-inicio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formulario-inicio',
+  standalone: true,
+  imports: [],
+  templateUrl: './formulario-inicio.component.html',
+  styleUrl: './formulario-inicio.component.css'
+})
+export class FormularioInicioComponent {
+
+}
--- frontend_facturacion/proyecto_jas/src/app/services/api-general.service.ts
+++ frontend_facturacion/proyecto_jas/src/app/services/api-general.service.ts
@@ -6,8 +6,9 @@ import { productos } from '../components/models/productos';
     providedIn: 'root'
 })
 export class ApiGeneralService {
-    urlApi: string = 'http://localhost:5200/api/facturacion';
+    urlApi: string = 'http://3.145.120.149:5200/api/facturacion';
     seLogueo: any = true
+
 
     constructor(private http: HttpClient) { }
 
